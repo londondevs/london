a30fd2d62ec828fa89b373c3caff5a59a44c26a9
londondevs <londondevs@vermine.nl>
2017-09-30 12:15:24 +0200

    Initial commit

---
+++ london-source/src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define LONDON-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "LONDONURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
